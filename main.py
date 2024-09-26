9b5a06becc22b6c712efc52978a1212fd104ccd1
Craig <121510295+CragglesG@users.noreply.github.com>
2024-09-26 14:08:02 +0100

    Finished main.py

--- main.py
+++ main.py
@@ -9,4 +9,7 @@ fmt_emails = []
 
 for email in emails:
     split_email = email.split("@")
-    fmt_emails.append()
+    fmt_emails.append(split_email[1])
+
+with open("emails.txt", "w") as f:
+    f.write('\n'.join(fmt_emails))
