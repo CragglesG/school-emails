25118384eca9f0cd4f08de5261bd1c0a7698cb5e
Craig <121510295+CragglesG@users.noreply.github.com>
2024-09-26 14:03:44 +0100

    updated

--- main.py
+++ main.py
@@ -2,3 +2,11 @@ import pandas
 
 with open("data.xlsx", "rb") as f:
     data = pandas.read_excel(f, sheet=1)
+
+emails = data["email"]
+
+fmt_emails = []
+
+for email in emails:
+    split_email = email.split("@")
+    fmt_emails.append()
