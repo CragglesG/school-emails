873ddaa20c31c5b3f06c6cf17eb4b43523539110
Craig <121510295+CragglesG@users.noreply.github.com>
2024-09-27 09:09:24 +0100

    Update verify_email.js

--- verify_email.js
+++ verify_email.js
@@ -4,10 +4,14 @@ const england_file = await Deno.readTextFile("england/emails.txt")
 const ireland = ireland_file.split('\n')
 const england = england_file.split('\n')
 
+const common = ["gmail.com", "eircom.net", "hotmail.com", "outlook.com", "outlook.ie", "yahoo.com"]
+
 function verify_email(email) {
     const email_domain = email.split("@").pop()
 
-    if (email_domain in ireland) {
+    if (email_domain in common) {
+        return "Fail"
+    } else if (email_domain in ireland) {
         return "Ireland"
     } else if (email_domain in england) {
         return "England"
