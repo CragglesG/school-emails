d237350f0cbc65d05eaa6f96d92a1359b21c4e3f
Restyled.io <commits@restyled.io>
2024-09-27 11:33:39 +0000

    Restyled by prettier

--- verify_email.js
+++ verify_email.js
@@ -1,30 +1,30 @@
-const ireland_file = await Deno.readTextFile("ireland/emails.txt")
-const england_file = await Deno.readTextFile("england/emails.txt")
+const ireland_file = await Deno.readTextFile("ireland/emails.txt");
+const england_file = await Deno.readTextFile("england/emails.txt");
 
-const ireland = ireland_file.split('\n')
-const england = england_file.split('\n')
+const ireland = ireland_file.split("\n");
+const england = england_file.split("\n");
 
-const common =
-    [
-      "gmail.com", "eircom.net", "hotmail.com", "outlook.com", "outlook.ie",
-      "yahoo.com"
-    ]
+const common = [
+  "gmail.com",
+  "eircom.net",
+  "hotmail.com",
+  "outlook.com",
+  "outlook.ie",
+  "yahoo.com",
+];
 
-    function verify_email(email) {
-      const email_domain = email.split("@").pop()
+function verify_email(email) {
+  const email_domain = email.split("@").pop();
 
-      if (common.includes(email_domain)) {
-        return "Fail"
-      }
-      else if (ireland.includes(email_domain)) {
-        return "Ireland"
-      }
-      else if (england.includes(email_domain)) {
-        return "England"
-      }
-      else {
-        return "Fail"
-      }
-    }
+  if (common.includes(email_domain)) {
+    return "Fail";
+  } else if (ireland.includes(email_domain)) {
+    return "Ireland";
+  } else if (england.includes(email_domain)) {
+    return "England";
+  } else {
+    return "Fail";
+  }
+}
 
-export default verify_email
+export default verify_email;
