78dcce596a572feb94499009d8f5cb0fb6a752dd
Restyled.io <commits@restyled.io>
2024-09-27 11:33:37 +0000

    Restyled by clang-format

--- verify_email.js
+++ verify_email.js
@@ -4,20 +4,27 @@ const england_file = await Deno.readTextFile("england/emails.txt")
 const ireland = ireland_file.split('\n')
 const england = england_file.split('\n')
 
-const common = ["gmail.com", "eircom.net", "hotmail.com", "outlook.com", "outlook.ie", "yahoo.com"]
+const common =
+    [
+      "gmail.com", "eircom.net", "hotmail.com", "outlook.com", "outlook.ie",
+      "yahoo.com"
+    ]
 
-function verify_email(email) {
-    const email_domain = email.split("@").pop()
+    function verify_email(email) {
+      const email_domain = email.split("@").pop()
 
-    if (common.includes(email_domain)) {
+      if (common.includes(email_domain)) {
         return "Fail"
-    } else if (ireland.includes(email_domain)) {
+      }
+      else if (ireland.includes(email_domain)) {
         return "Ireland"
-    } else if (england.includes(email_domain)) {
+      }
+      else if (england.includes(email_domain)) {
         return "England"
-    } else {
+      }
+      else {
         return "Fail"
+      }
     }
-}
 
 export default verify_email
