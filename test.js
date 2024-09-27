5340799a7ba58bd479a819bb1f36db2d829519dc
Craig <121510295+CragglesG@users.noreply.github.com>
2024-09-27 08:33:17 +0100

    Create test.js

---
+++ test.js
@@ -0,0 +1,12 @@
+import { expect } from "jsr:@std/expect";
+import verify_email from "verify_email.js";
+
+Deno.test("Test for Irish email", () => {
+  const result = verify_email("cgiles.stu@naascc.ie");
+  expect(result).toBe("Ireland");
+});
+
+Deno.test("Test for non-educational email", () => {
+  const result = verify_email("contact@cobraracing.ie");
+  expect(result).toBe("Fail");
+});
